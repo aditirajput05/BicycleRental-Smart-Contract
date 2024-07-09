88b70c25d3645427984c7a310fa42c816d27f5d8
aditirajput05 <133592554+aditirajput05@users.noreply.github.com>
2024-07-10 01:27:50 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,16 @@
+const { ethers } = require("hardhat");
 
+async function main() {
+  const BicycleRental = await ethers.getContractFactory("BicycleRental");
+  const bicycleRental = await BicycleRental.deploy();
+  await bicycleRental.deployed();
+
+  console.log(`BicycleRental deployed to: ${bicycleRental.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
